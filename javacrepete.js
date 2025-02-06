5704103d93d51434d0da55bc057ca7d31ba36699
LUCAS RODRIGUES NOBRE SILVA <24122499@CFP127ALUNO.LOCAL>
2025-02-06 14:20:24 -0300

    ajudaaquiprofessor

---
+++ javacrepete.js
@@ -0,0 +1,10 @@
+"use strict"
+
+function criarMenu (){
+    const novoItem = document.createElement('li')
+    const novaImagem = document.createElement('img')
+    const novoRego = document.createElement('span')
+    const novoBoga = document.createElement('menu')
+}
+
+criarMenu
