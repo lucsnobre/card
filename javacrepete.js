28632faf93a4b0bd9215acf2add03ff773542dbe
LUCAS RODRIGUES NOBRE SILVA <24122499@CFP127ALUNO.LOCAL>
2025-02-06 14:50:46 -0300

    voltanohtmlprofessor

--- javacrepete.js
+++ javacrepete.js
@@ -1,10 +1,25 @@
 "use strict"
 
-function criarMenu (){
+const categorias = [
+    {nome: 'celular', icon: 'celular.png'},
+    {nome: 'informatica', icon: 'informatica.png'},
+    {nome: 'games', icon: 'controle.png'},
+    {nome: 'moveis', icon: 'moveis.png'}
+]
+
+function criarMenu (categoria){
     const novoItem = document.createElement('li')
     const novaImagem = document.createElement('img')
-    const novoRego = document.createElement('span')
-    const novoBoga = document.createElement('menu')
+    const novoSpan = document.createElement('span')
+    const lista = document.getElementById('menu')
+
+    novaImagem.src = `./img/${categoria.icon}`
+    novoSpan.textContent = categoria.nome
+
+    novoItem.appendChild(novaImagem)
+    novoItem.appendChild(novoSpan)
+
+    lista.appendChild(novoItem)
 }
 
-criarMenu
+categorias.forEach(criarMenu)
